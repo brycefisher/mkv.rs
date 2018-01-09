9e12c046539bfda3de40ad2e99d8b4546ec26dd1
Bryce Fisher-Fleig <hckr1292@gmail.com>
2018-01-09 12:49:25 -0500

    (nit) Remove dead code

--- examples/dom_dumper.rs
+++ examples/dom_dumper.rs
@@ -13,12 +13,9 @@ use mkv::elements::parser::EventsHandler;
 
 extern crate mkv;
 extern crate log;
-extern crate env_logger;
 
 const BSIZE : usize = 4096;
 fn main() {
-    env_logger::init().unwrap();
-
     let mut reader : Box<Read> = match args().len() {
         1 => Box::new(std::io::stdin()),
         2 => Box::new(File::open(Path::new(args().nth(1).unwrap().as_str())).expect("Failed to open the file")),
@@ -27,8 +24,6 @@ fn main() {
     let mut f = BufReader::new(reader);
 
 
-    //let mut stdout = std::io::stdout();
-    let element_logger = mkv::elements::parser::debug::DebugPrint::new(log::LogLevel::Info);
     let mut dom_builder : mkv::elements::builder::Builder = Default::default();
     {
         let mut m = mkv::elements::parser::new();
